d469d51d7f381eac538140e8a746669a9a1d7137
nanthanavalli <nanthanavalli0309@gmail.com>
2024-09-25 22:55:20 +0530

    Seperate login pages are added

--- frontend/src/pages/common/LoginPage.jsx
+++ frontend/src/pages/common/LoginPage.jsx
@@ -2,26 +2,44 @@ import styles from "./loginPage.module.css";
 import Nav from "../../components/common/Nav";
 import CustomTextInput from "../../components/common/CustomTextInput";
 import CustomButton from "../../components/common/CustomButton";
-import {useState} from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    return <div className={styles.loginPage}>
-        <div className={styles.content}>
-            <div className={styles.formContainer}>
-                <h1 className={styles.loginText}>Login</h1>
-                <CustomTextInput placeholder="Username"
-                                 value={username}
-                                 onChange={setUsername}/>
-                <CustomTextInput placeholder="Password"
-                                 value={password}
-                                 onChange={setPassword}/>
-                <CustomButton label="Submit"/>
-            </div>
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.loginPage}>
+      <div className={styles.content}>
+        <div className={styles.formContainer}>
+          <h1 className={styles.loginText}>Login</h1>
+          <CustomTextInput
+            placeholder="Username"
+            value={username}
+            onChange={setUsername}
+          />
+          <CustomTextInput
+            placeholder="Password"
+            value={password}
+            onChange={setPassword}
+          />
+          <CustomButton
+            onClick={() => {
+              if (username === "student") {
+                navigate("/student/home");
+              }else if(username ==="admin"){
+                navigate("/admin/home");
+              }
+            }}
+            label="Submit"
+          />
         </div>
+      </div>
     </div>
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
